6b28d9eace095196c8f79be6fafb698d87e531dd
Aj <btchaggler@gmail.com>
2014-09-29 16:32:00 +0000

    insert and query

--- Main.hs
+++ Main.hs
@@ -8,9 +8,10 @@
     FlexibleContexts #-}
 
 import Control.Monad.IO.Class (liftIO)
-import Database.Persist
+import Database.Persist as Per
 import Database.Persist.Sqlite (runSqlite, runMigration)
 import Database.Persist.TH
+import Data.Maybe (fromJust)
 
 share [mkPersist sqlSettings, mkMigrate "migrateTables"] [persistLowerCase|
 Person
@@ -28,9 +29,12 @@ main = runSqlite ":memory:" $ do
     runMigration migrateTables
     johnid <- insert $ Person "John Doe" (Just 30)
     janeid <- insert $ Person "Jane bab" Nothing
+    insert_ $ Person "Blade raj" (Just 27)
     insert $ BlogPost "Yea daddy" johnid
     insert $ BlogPost "Bye man" janeid
     johnpost <- selectList [BlogPostAuthorId ==. johnid] [LimitTo 1]
+    bladeraj <- selectFirst [Filter PersonName (Right ["Blade raj"]) Per.Eq] []
+    liftIO $ print $ entityVal $ fromJust bladeraj
     john <- get johnid
     liftIO $ print (john :: Maybe Person)
     delete janeid
